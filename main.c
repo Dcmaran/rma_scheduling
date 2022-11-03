fffbd652bb11f27d6a1f2db54d761edfa2b038d9
diogo <diogocmr2002@gmail.com>
2022-11-03 08:47:37 -0300

    idle

--- main.c
+++ main.c
@@ -42,8 +42,8 @@ int main(int argc, char *argv[])
 
     for (int total_time_count = 1; total_time_count <= total_time; total_time_count++)
     {
-        printf("%d\n", total_time_count);
-        printf("[%s] %d %d\n", process_list[index_process].process_name, process_list[index_process].executed_total, process_list[index_process].executed_ut);
+        //printf("%d\n", total_time_count);
+        //printf("[%s] %d %d\n", process_list[index_process].process_name, process_list[index_process].executed_total, process_list[index_process].executed_ut);
         if (aux < idle_count)
         {
             aux++;
@@ -154,7 +154,7 @@ int main(int argc, char *argv[])
                                
             if (process_list[index_process].period_count == process_list[index_process].period)
             {
-                if (process_list[index_process].executed_total < process_list[index_process].execution_time)
+                if (process_list[index_process].executed_total < process_list[index_process].execution_time && process_list[index_process].execution_time - process_list[index_process].executed_total + total_time_count < total_time)
                 {
                     printf("[%s] for %d units - L\n", process_list[index_process].process_name, process_list[index_process].execution_time - process_list[index_process].executed_total);
                     process_list[index_process].executed_total = 0;
